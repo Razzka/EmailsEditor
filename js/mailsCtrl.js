9ddd0abf34f2c52eaedd1f8f8ab1da1c54a7e705
Aleksandr <Razko90@mail.ru>
2016-10-04 22:56:56 +0500

    Commiting all files

---
+++ js/mailsCtrl.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var app = angular.module('emailsApp', ['emailsEditor']);
+
+app.controller('mailsCtrl', ['$scope', function($scope) {
+    $scope.mails = [];
+    
+    function generateRandomString() {
+        var lengthPrefix = Math.floor(Math.random() * 5 + 2);
+        var lengthPostfix = Math.floor(Math.random() * 5 + 2);
+        return Math.random().toString(36).substring(2, lengthPrefix) + '@' + Math.random().toString(36).substring(2, lengthPostfix) + '.ru';
+    }
+    
+    $scope.addEmails = function() {
+        $scope.mails.push(generateRandomString());   
+    };
+    
+    $scope.getEmailsCount = function() {
+        alert($scope.mails.length + ' mails');
+    };
+}]);
